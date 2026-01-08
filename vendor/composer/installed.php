4c50f50a97ca218796d15c212de38c367f5f50aa
Magal <magal@pixelsolutions.biz>
2026-01-08 16:36:52 +0100

    introducing kassner/log-parser

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2223aade944da41071da713b549a2e84edc2045a',
+        'name' => 'logwatch/cmsms-module',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kassner/log-parser' => array(
+            'pretty_version' => '1.5.0',
+            'version' => '1.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kassner/log-parser',
+            'aliases' => array(),
+            'reference' => 'ea846b7edf24a421c5484902b2501c9c8e065796',
+            'dev_requirement' => false,
+        ),
+        'logwatch/cmsms-module' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2223aade944da41071da713b549a2e84edc2045a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
